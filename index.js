1ac123b7de29d576503fc12f3a0ff6f4a06b0cc0
xristosstamos <xristoss.stamoss@gmail.com>
2022-12-22 11:16:42 +0200

    comment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ setInterval(setClock, 1000)
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
+//just a comment
 
 function setClock() {
     const currentDate = new Date()
